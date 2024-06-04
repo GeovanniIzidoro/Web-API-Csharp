debd3ae9bb18d96f99230200f46422c611be9a0c
Geovanni Izidoro <geovanniizi2002@hotmail.com>
2024-06-04 18:35:57 -0300

    first commit

---
+++ WebAPI-C#/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_C_.Models;
+
+namespace WebAPI_C_.DataContext
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {   
+        }
+
+        
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
---
+++ WebAPI-C#/Enums/DepartamentoEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebAPI_C_.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum DepartamentoEnum
+    {
+        RH,
+        Financeiro,
+        Compras,
+        Atendimento,
+        Zeladoria
+    }
+}
